14ec452385e67c8b8c8b154188dee2b0a73c192d
marwamoussafir74 <marwamoussafir74@gmail.com>
2022-05-08 13:18:53 +0200

    progs10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { CompletedComponent } from './components/completed/completed.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+
+const routes:Route[] =[
+  {
+    path: "",
+    component: TodoComponent
+  },
+  {
+    path:"completed",
+    component: CompletedComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    CompletedComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { };
